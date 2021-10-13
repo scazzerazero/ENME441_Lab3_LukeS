9900a2c2955fb54864f8e1baf573989ff905a093
Luke Scazzero <91499364+scazzerazero@users.noreply.github.com>
2021-10-13 19:31:30 +0000

    Added xval and yval attributes to joystick class for clarity

--- main.py
+++ main.py
@@ -5,19 +5,18 @@ import time
 
 class PCF8591:
 
-  def __init__(self,address): #the eslf has attritubtes. Here it only has one.
-    #self.___ represents the instance of the class. By using the “self” keyword we can access the attributes and methods of the class in python. It binds the attributes with the given arguments. The reason you need to use self. is because Python does not use the @ syntax to refer to instance attributes.
+  def __init__(self,address): #the "self" has attritubtes. Here it only has one, an address.
+    #self.___ represents the instance of the class. By using the “self” keyword we can access the attributes and methods of the class in python. It binds the attributes with the given arguments. The reason you need to use self is because Python does not use the @ syntax to refer to instance attributes.
     self.bus = smbus.SMBus(1) #newer Pis use bus no.1
     self.address = address #the I2C device lives at a address in the bus
-  #the self can do things! Creating our read() and write() methods:
+  #the "self" can do things! Here we're creating our read() and write() methods for it to use:
   def read(self,chn): #channel
       try:
           # Read/write 1 byte from/to a specific register of a given device address
-          #note about the control byte for ADC:
-            #0,analog out enabled?,analog in channel select,analog in channel select,0,A/D channel number 0 1 2 or 3
           self.bus.write_byte(self.address, 0x40 | chn)  # 01000000
-
           self.bus.read_byte(self.address) # dummy read to start conversion
+          #note about the 8 control bits for ADC:
+            #0,analog out enabled?,analog in channel select,analog in channel select,0,A/D channel number 0 1 2 or 3
       except Exception as e:
           print ("Address: %s \n%s" % (self.address,e))
       return self.bus.read_byte(self.address)
@@ -30,14 +29,18 @@ class PCF8591:
 
 
 
-class Joystick:
-  def __init__(self,address):
-    self.ADC=PCF8591(address)
+class Joystick: #creating joystick class
+  def __init__(self,ADCaddress,Xval,Yval): #giving our class attributes 
+    self.ADC=PCF8591(ADCaddress) # By composition, we're extending the PCF8591 class. using it to define our Joystick class's attribute self.ADC
+    self.Xval= Xval #other attributes
+    self.Yval= Yval
 
-  def getX(self):
-    return self.ADC.read(1) #read channel one
+  def getX(self): #a method to getX reading
+    Xval =self.ADC.read(1) # val from ADC chn 1. depends on where you plugged in joystick wires
+    return Xval
   def getY(self):
-    return self.ADC.read(2) #read channel two
+    Yval = self.ADC.read(2) # val from ADC chn 2
+    return Yval
 
 JoystickObject=Joystick(0x48)
 while 1:
