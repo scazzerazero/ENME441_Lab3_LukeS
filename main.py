5e9e7489684716769d15fce9e3cbb765eb75fcd3
Luke Scazzero <91499364+scazzerazero@users.noreply.github.com>
2021-10-13 19:03:29 +0000

    add keyboard interrupt break

--- main.py
+++ main.py
@@ -45,7 +45,8 @@ while 1:
     print("{:>3},{:>3}".format(JoystickObject.getX(), JoystickObject.getY())) #thanks gavin and hannah!
     time.sleep(0.1)
   except KeyboardInterrupt:
-      print("\nEXITING!")
+    print("\nEXITING!")
+    break
 
 
 
